295e4b077cfe6d76d7c4c0881fe89651d34b7e29
Kaio Pomini <kaiopomini@gmail.com>
2019-07-13 15:13:29 -0300

    Promotion (Pawn)

--- chess-java/src/application/Program.java
+++ chess-java/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -40,12 +41,25 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e){
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 			catch(InputMismatchException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InvalidParameterException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(IllegalStateException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
